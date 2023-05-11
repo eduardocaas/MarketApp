61f4c31b648896610479aa376ade80bd89648dd3
eduardocaas <eduardofc.dev@gmail.com>
2023-05-10 23:04:19 -0300

    Repository for ItemSale

--- src/main/java/com/efc/repository/ItemSaleRepository.java
+++ src/main/java/com/efc/repository/ItemSaleRepository.java
@@ -2,6 +2,8 @@ package com.efc.repository;
 
 import com.efc.entity.ItemSale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemSaleRepository extends JpaRepository<ItemSale, Long> {
 }
