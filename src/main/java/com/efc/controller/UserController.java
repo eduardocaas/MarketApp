646b9a24fd01fa21d3e6d3263d179a4fec240fd9
eduardocaas <eduardofc.dev@gmail.com>
2023-05-11 23:09:11 -0300

    Controller for User with GetById and GetAll and Save methods

---
+++ src/main/java/com/efc/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.efc.controller;
+
+import com.efc.entity.User;
+import com.efc.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+  private UserRepository repository;
+
+  public UserController(@Autowired UserRepository repository) {
+    this.repository = repository;
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity getById(@PathVariable Long id){
+    try {
+      return ResponseEntity.ok().body(repository.findById(id));
+    } catch (Exception error) {
+      return ResponseEntity.internalServerError().body(error.getMessage());
+    }
+  }
+
+  @GetMapping
+  public ResponseEntity getAll(){
+    return ResponseEntity.ok().body(this.repository.findAll());
+  }
+
+  @PostMapping
+  public ResponseEntity save(@RequestBody User user){
+    try {
+      User obj = repository.save(user);
+      URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+      return ResponseEntity.created(uri).build();
+    } catch (Exception error) {
+      return ResponseEntity.internalServerError().body(error.getMessage());
+    }
+  }
+}
