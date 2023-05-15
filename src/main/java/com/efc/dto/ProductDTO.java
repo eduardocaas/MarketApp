c927387b0023e13cef8fa395fe49f914aae25679
eduardocaas <eduardofc.dev@gmail.com>
2023-05-14 23:42:20 -0300

    DTO for Product

---
+++ src/main/java/com/efc/dto/ProductDTO.java
@@ -0,0 +1,15 @@
+package com.efc.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+
+    private Long product_id;
+    private Integer quantity;
+
+}
