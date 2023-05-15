c756b64d3d9f6a551dbfa3b105ac49b2f870c66f
eduardocaas <eduardofc.dev@gmail.com>
2023-05-14 23:43:15 -0300

    Methods for Product Controller

--- src/main/java/com/efc/controller/ProductController.java
+++ src/main/java/com/efc/controller/ProductController.java
@@ -1,19 +1,24 @@
 package com.efc.controller;
 
+import com.efc.entity.Product;
+import com.efc.entity.User;
 import com.efc.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
-  private ProductRepository repository;
+  private final ProductRepository repository;
 
-  public ProductController(@Autowired ProductRepository repository) {
+  @Autowired
+  public ProductController(ProductRepository repository) {
     this.repository = repository;
   }
 
@@ -21,5 +26,46 @@ public class ProductController {
   public ResponseEntity getAll() {
     return ResponseEntity.ok().body(repository.findAll());
   }
+
+  @GetMapping("/{id}")
+  public ResponseEntity findById(@PathVariable("id") Long id) {
+    Optional<Product> obj = repository.findById(id);
+    if (obj.isPresent()) {
+      return ResponseEntity.ok().body(obj);
+    }
+    return ResponseEntity.notFound().build();
+  }
+
+  @PostMapping
+  public ResponseEntity save(@RequestBody Product product) {
+    try {
+      Product obj = repository.save(product);
+      URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+      return ResponseEntity.created(uri).build();
+    } catch (Exception error) {
+      return ResponseEntity.internalServerError().body(error.getMessage());
+    }
+  }
+
+  @PutMapping
+  public ResponseEntity update(@RequestBody Product product) {
+    Optional<Product> obj = repository.findById(product.getId());
+    if(obj.isPresent()) {
+      repository.save(product);
+      return ResponseEntity.ok().body(product);
+    }
+    return ResponseEntity.notFound().build();
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity delete(@PathVariable("id") Long id) {
+    try {
+      repository.deleteById(id);
+      return ResponseEntity.noContent().build();
+    } catch (Exception error) {
+      return ResponseEntity.internalServerError().body(error.getMessage());
+    }
+  }
+
 
 }
