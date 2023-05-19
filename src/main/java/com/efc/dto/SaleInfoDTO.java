4fd1fb93fadcacd94a08e8b2e825162609ce7bc4
eduardocaas <eduardofc.dev@gmail.com>
2023-05-18 23:38:03 -0300

    DTO for info

---
+++ src/main/java/com/efc/dto/SaleInfoDTO.java
@@ -0,0 +1,18 @@
+package com.efc.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleInfoDTO {
+
+    private String user;
+    private String date;
+    private List<ProductInfoDTO> products;
+
+}
