220e966297d5e6044c439fb69daba1e154f45006
eduardocaas <eduardofc.dev@gmail.com>
2023-05-19 23:45:56 -0300

    Catch exception for Product error

--- src/main/java/com/efc/controller/SaleController.java
+++ src/main/java/com/efc/controller/SaleController.java
@@ -1,8 +1,10 @@
 package com.efc.controller;
 
 import com.efc.dto.SaleDTO;
+import com.efc.exception.ProductException;
 import com.efc.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -25,12 +27,19 @@ public class SaleController {
         return ResponseEntity.ok().body(service.getAll());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity getById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(service.getById(id));
+    }
+
     @PostMapping
     public ResponseEntity save(@RequestBody SaleDTO saleDTO) {
         try {
             Long id = service.save(saleDTO);
             URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
             return ResponseEntity.created(uri).build();
+        } catch (ProductException productError) {
+            return new ResponseEntity(productError.getMessage(), HttpStatus.NO_CONTENT);
         } catch (Exception error) {
             return ResponseEntity.internalServerError().body(error.getMessage());
         }
