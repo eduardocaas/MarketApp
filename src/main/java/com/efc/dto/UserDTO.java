df306a74339a15545c763c1394932fbf6a97eaf5
eduardocaas <eduardofc.dev@gmail.com>
2023-05-20 17:37:48 -0300

    DTO for User

---
+++ src/main/java/com/efc/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package com.efc.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private Boolean isEnabled;
+
+}
