c7f9a96eb27ca3024e2a2c19c0b5ae6b25f2e4f6
eduardocaas <eduardofc.dev@gmail.com>
2023-05-25 22:06:12 -0300

    Fields validation

--- src/main/java/com/efc/entity/Product.java
+++ src/main/java/com/efc/entity/Product.java
@@ -1,6 +1,9 @@
 package com.efc.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,12 +22,16 @@ public class Product {
   private Long id;
 
   @Column(length = 150, nullable = false)
+  @NotBlank(message = "Description field is required")
   private String description;
 
   @Column(length = 20, precision = 20, scale = 2, nullable = false)
+  @NotNull(message = "Price field is required")
   private BigDecimal price;
 
   @Column(nullable = false)
+  @NotNull(message = "Quantity field is required")
+  @Min(value = 1, message = "Minimum quantity is 1")
   private Integer quantity;
 
 }
