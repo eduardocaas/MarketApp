2d3ca66c54c59c4cc056a3451a564f6176d4f1b2
eduardocaas <eduardofc.dev@gmail.com>
2023-05-27 22:40:27 -0300

    ProductSaleDTO validation

--- src/main/java/com/efc/dto/ProductSaleDTO.java
+++ src/main/java/com/efc/dto/ProductSaleDTO.java
@@ -1,5 +1,6 @@
 package com.efc.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProductSaleDTO {
 
+    @NotNull(message = "Product is required")
     private Long product_id;
+    @NotNull(message = "Quantity is required")
     private Integer quantity;
 
 }
