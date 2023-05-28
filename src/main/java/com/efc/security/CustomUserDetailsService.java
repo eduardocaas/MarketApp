9bd6e2df027885ce90207e89520820a4b2790816
eduardocaas <eduardofc.dev@gmail.com>
2023-05-27 23:16:41 -0300

    User security

---
+++ src/main/java/com/efc/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.efc.security;
+
+import com.efc.entity.User;
+import com.efc.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getByUsername(username);
+        return new UserPrincipal(user);
+    }
+}
