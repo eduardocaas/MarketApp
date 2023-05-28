b904e71f56e8e1864705a664e7a857e0260db6cb
eduardocaas <eduardofc.dev@gmail.com>
2023-05-27 22:41:34 -0300

    Validation for UserDTO

--- src/main/java/com/efc/dto/UserDTO.java
+++ src/main/java/com/efc/dto/UserDTO.java
@@ -1,6 +1,8 @@
 package com.efc.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,10 +11,16 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class UserDTO {
 
     private Long id;
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotBlank(message = "Username is required")
+    private String username;
+    @NotBlank(message = "Password is required")
+    private String password;
     private Boolean isEnabled;
 
 }
