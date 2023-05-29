b5acd0533427b33acc61fbcecffc7b745a872a7d
eduardocaas <eduardofc.dev@gmail.com>
2023-05-28 23:35:02 -0300

    Fix imports

--- src/main/java/com/efc/service/DBService.java
+++ src/main/java/com/efc/service/DBService.java
@@ -2,9 +2,7 @@ package com.efc.service;
 
 import com.efc.dto.UserDTO;
 import com.efc.entity.Product;
-import com.efc.entity.User;
 import com.efc.repository.ProductRepository;
-import com.efc.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
