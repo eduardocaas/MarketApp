58ee199393f63a5ad2d51e4cd3c22401cf7d4ee1
eduardocaas <eduardofc.dev@gmail.com>
2023-05-28 23:36:43 -0300

    findUserByUsername throwing error, fix using creating query

--- src/main/java/com/efc/repository/UserRepository.java
+++ src/main/java/com/efc/repository/UserRepository.java
@@ -2,11 +2,14 @@ package com.efc.repository;
 
 import com.efc.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findUserByUsername(String username);
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.sales WHERE u.username = :username")
+    User findUserByUsername(@Param("username") String username);
 
 }
