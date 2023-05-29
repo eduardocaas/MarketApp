3cafce15f74d34bd88bd2cd8977560d0ee58af18
eduardocaas <eduardofc.dev@gmail.com>
2023-05-28 23:35:49 -0300

    Info route to unauthenticated users

--- src/main/java/com/efc/security/SecurityConfig.java
+++ src/main/java/com/efc/security/SecurityConfig.java
@@ -3,14 +3,11 @@ package com.efc.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -31,14 +28,14 @@ public class SecurityConfig {
 
         http
             .authorizeHttpRequests()
-                .anyRequest()
-                .authenticated()
+                .requestMatchers("/info").permitAll()
+                .anyRequest().authenticated()
             .and()
                 .httpBasic();
 
          http
             .csrf()
-                .disable();
+                .disable(); //Apenas em ambiente de dev
 
         http
            .headers()
