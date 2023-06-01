e4a997b4b8288c8b500275766c958692ec3597fb
eduardocaas <eduardofc.dev@gmail.com>
2023-06-01 17:32:20 -0300

    Implemented method for check password and username

--- src/main/java/com/efc/security/CustomUserDetailsService.java
+++ src/main/java/com/efc/security/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.efc.security;
 
+import com.efc.dto.LoginDTO;
 import com.efc.entity.User;
+import com.efc.exception.PasswordNotFoundException;
 import com.efc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,6 +19,22 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userService.getByUsername(username);
+
+        if(user == null) {
+            throw new UsernameNotFoundException("Invalid login");
+        }
+
         return new UserPrincipal(user);
+    }
+
+    public void verifyUserCredentials(LoginDTO login) {
+        UserDetails user = loadUserByUsername(login.getUsername());
+        boolean passwordIsEqual =  SecurityConfig
+                                    .passwordEncoder()
+                                    .matches(login.getPassword(), user.getPassword());
+
+        if(!passwordIsEqual) {
+            throw new PasswordNotFoundException("Invalid password");
+        }
     }
 }
