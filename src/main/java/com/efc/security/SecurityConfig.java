365649abb5a3101d9afe9d0ce2febba379454943
eduardocaas <eduardofc.dev@gmail.com>
2023-06-02 11:32:47 -0300

    Info and Login endpoints with defined Http methods

--- src/main/java/com/efc/security/SecurityConfig.java
+++ src/main/java/com/efc/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.efc.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,7 +29,8 @@ public class SecurityConfig {
 
         http
             .authorizeHttpRequests()
-                .requestMatchers("/info").permitAll()
+                .requestMatchers(HttpMethod.GET, "/info").permitAll()
+                .requestMatchers(HttpMethod.POST, "/login").permitAll()
                 .anyRequest().authenticated()
             .and()
                 .httpBasic();
